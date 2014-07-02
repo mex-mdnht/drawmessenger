e90439dce00befbffcaf78b9630e8493fda9720c
maeda <maeda@taki-mex.jp>
2014-07-02 23:46:18 +0900

    handheld companion app implemented

--- mobile/src/main/java/jp/mdnht/drawmessenger/MockActivity.java
+++ mobile/src/main/java/jp/mdnht/drawmessenger/MockActivity.java
@@ -1,22 +1,60 @@
 package jp.mdnht.drawmessenger;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
 import android.view.View;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.NotificationCompat.WearableExtender;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.Collection;
+import java.util.HashSet;
 
 
-public class MockActivity extends Activity {
+public class MockActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, MessageApi.MessageListener, NodeApi.NodeListener {
 
     private static final String TAG = "MockActivity";
+
+    /** Request code for launching the Intent to resolve Google Play services errors. */
+    private static final int REQUEST_RESOLVE_ERROR = 1000;
+
+    private static final String START_ACTIVITY_PATH = "/start-activity";
+
+    //google api
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mResolvingError = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mock);
-        LOGD(TAG,"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+
+
+         mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
 
 
@@ -25,6 +63,15 @@ public class MockActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.mock, menu);
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        super.onStart();
+        if (!mResolvingError) {
+            mGoogleApiClient.connect();
+        }
     }
 
     @Override
@@ -41,6 +88,109 @@ public class MockActivity extends Activity {
 
     public void onSendNotificationClicked(View view) {
         LOGD(TAG,"sendNotificationClicked");
+        createNotificationAndSend();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        LOGD(TAG,"nnnnnnnnnnnnnnwwww intnt");
+        super.onNewIntent(intent);
+        new StartWearableActivityTask().execute();
+
+    }
+
+    private void createNotificationAndSend(){
+        int notificationId = 001;
+
+        //main notification
+        NotificationCompat.Builder notifBulder = new NotificationCompat.Builder(this)
+                .setContentTitle("メッセージ")
+                .setContentText("ひらく")
+                .setSmallIcon(R.drawable.common_signin_btn_icon_dark);
+
+        //extender for page2
+        WearableExtender extender2 = new WearableExtender()
+                .setHintShowBackgroundOnly(true)
+                .setBackground(BitmapFactory.decodeResource(this.getResources(),
+                        R.drawable.ic_plusone_medium_off_client));
+        // Create second page notification
+        Notification secondPageNotification = new NotificationCompat.Builder(this)
+                .setContentTitle("pege2")
+                .setContentText("test")
+                .extend(extender2)
+                .build();
+
+        // Create an intent for the reply action
+        Intent actionIntent = new Intent(this, MockActivity.class);
+        PendingIntent actionPendingIntent =
+                PendingIntent.getActivity(this, 0, actionIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Create the action
+        NotificationCompat.Action action =
+                new NotificationCompat.Action.Builder(R.drawable.common_signin_btn_icon_disabled_focus_light,"launch wear app", actionPendingIntent)
+                        .build();
+
+
+        // Create a WearableExtender to add functionality for wearables
+        Notification notif =
+                new WearableExtender()
+                        .addPage(secondPageNotification)
+                        .addAction(action)
+                        .extend(notifBulder)
+                        .build();
+
+
+
+
+
+
+        // Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+        // Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notif);
+    }
+
+    @Override
+    public void onPeerConnected(final Node peer) {
+        LOGD(TAG, "onPeerConnected: " + peer);
+    }
+
+    @Override
+    public void onPeerDisconnected(final Node peer) {
+        LOGD(TAG, "onPeerDisconnected: " + peer);
+    }
+
+    private class StartWearableActivityTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... args) {
+            LOGD(TAG,"nnnnnnnnnnnnnnwwww dddddddddddddd");
+            Collection<String> nodes = getNodes();
+            LOGD(TAG,"node is"+nodes.toString());
+            for (String node : nodes) {
+                sendStartActivityMessage(node);
+            }
+            return null;
+        }
+    }
+
+    private void sendStartActivityMessage(String node) {
+        LOGD(TAG,"send start activity message");
+        Wearable.MessageApi.sendMessage(
+                mGoogleApiClient, node, START_ACTIVITY_PATH, new byte[0]).setResultCallback(
+                new ResultCallback<MessageApi.SendMessageResult>() {
+                    @Override
+                    public void onResult(MessageApi.SendMessageResult sendMessageResult) {
+                        if (!sendMessageResult.getStatus().isSuccess()) {
+                            Log.e(TAG, "Failed to send message with status code: "
+                                    + sendMessageResult.getStatus().getStatusCode());
+                        }
+                    }
+                }
+        );
     }
 
     /**
@@ -50,5 +200,63 @@ public class MockActivity extends Activity {
         if (Log.isLoggable(tag, Log.INFO)) {
             Log.d(tag, message);
         }
+    }
+
+    //GoogleApiClient
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        LOGD(TAG, "Google API Client was connected");
+        mResolvingError = false;
+        //mStartActivityBtn.setEnabled(true);
+       // Wearable.DataApi.addListener(mGoogleApiClient, this);
+        Wearable.MessageApi.addListener(mGoogleApiClient, this);
+        Wearable.NodeApi.addListener(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        LOGD(TAG, "Connection to Google API client was suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (mResolvingError) {
+            // Already attempting to resolve an error.
+            return;
+        } else if (result.hasResolution()) {
+            try {
+                mResolvingError = true;
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                // There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
+            }
+        } else {
+            Log.e(TAG, "Connection to Google API client has failed");
+            mResolvingError = false;
+            //mStartActivityBtn.setEnabled(false);
+            //mSendPhotoBtn.setEnabled(false);
+            //Wearable.DataApi.removeListener(mGoogleApiClient, this);
+            Wearable.MessageApi.removeListener(mGoogleApiClient, this);
+            Wearable.NodeApi.removeListener(mGoogleApiClient, this);
+        }
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        LOGD(TAG, "onMessageReceived() A message from watch was received:" + messageEvent
+                .getRequestId() + " " + messageEvent.getPath());
+    }
+
+    private Collection<String> getNodes() {
+        LOGD(TAG,"nnnnnnodes gewt");
+        HashSet <String>results= new HashSet<String>();
+        NodeApi.GetConnectedNodesResult nodes =
+                Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
+        for (Node node : nodes.getNodes()) {
+            results.add(node.getId());
+        }
+        LOGD(TAG,"nnnnnnodes2 gewt"+results.toString());
+        return results;
     }
 }
